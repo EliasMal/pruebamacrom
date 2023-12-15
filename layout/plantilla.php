95de49e54f8767f85976d73eaafb3d0776371d72
EliasMal <elias@macromautopartes.com>
2023-12-15 15:03:18 -0600

    s

--- layout/plantilla.php
+++ layout/plantilla.php
@@ -148,7 +148,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
 <!--===============================================================================================-->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="sweetalert2.all.min.js"></script>
         
 <!--===============================================================================================-->
 	<script type="text/javascript" src="vendor/parallax100/parallax100.js"></script>
